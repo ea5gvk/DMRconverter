5d8ff05c70a0ac42800cb24f8b1ff02557069b62
Cort Buffington <n0mjs@me.com>
2014-11-17 15:55:06 -0600

    Update DMRconverter.py

--- DMRconverter.py
+++ DMRconverter.py
@@ -1,3 +1,14 @@
+#!/usr/bin/env python
+#
+# Copyright (c) 2014, N0MJS and the K0USY Group.
+#
+# This work is licensed under the Creative Commons Attribution-ShareAlike
+# 3.0 Unported License.To view a copy of this license, visit
+# http://creativecommons.org/licenses/by-sa/3.0/ or send a letter to
+# Creative Commons, 444 Castro Street, Suite 900, Mountain View,
+# California, 94041, USA.
+
+
 import re
 import sys
 
