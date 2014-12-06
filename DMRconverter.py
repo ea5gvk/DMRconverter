75104de0213c54d8ddf447a71508a43e16739872
Cort Buffington <n0mjs@me.com>
2014-12-06 10:40:03 -0600

    MUCH IMPROVED

--- DMRconverter.py
+++ DMRconverter.py
@@ -11,6 +11,7 @@
 
 import re
 import sys
+import socket
 
 def id_to_ip (_id):
     _ip = list(re.findall('..', hex(_id)[2:].rjust(6,'0')))
@@ -25,31 +26,52 @@ def ip_to_id (_ip):
     _id = int(''.join(["%02X" % long(octet) for octet in _id]), 16)
     return _id
 
-print "DMR Radio ID to IP Address and Radio ID to IP Address Converter"
-print "  enter a radio ID or IP address to convert, or a"
-print "  the enter/return key to quit"
+def convert(_item):
+
+    try:
+        if (int(_item) < 16777216): 
+            ip = id_to_ip(int(_item))
+            radio_ip = '12.' + ip
+            pc_ip = '13.' + ip
+            print "Radio ID:                ", _item
+            print "Radio IP adddress:       ", radio_ip
+            print "PC IP address:           ", pc_ip
+            print ""
+        else:
+            print "Not a valid radio ID or IP address:  ", _item
+            print ""
+    except ValueError:
+        try:
+            socket.inet_aton(_item)
+            id = ip_to_id(_item)
+            print "IP addresss:             ", _item
+            print "Radio ID is:             ", id 
+            print ""
+        except socket.error:
+            print "Not a valid radio ID or IP address:  ", _item
+            print ""
+
 print ""
 
-while True:
-    input = raw_input("Radio ID or IP Address to Convert? ")
-    if not input:
-        sys.exit()
+if len(sys.argv) > 1:
+    for item in sys.argv[1:]:
+        convert(item)
+else:
+    print "  enter a radio ID or IP address to convert, or a"
+    print "  the enter/return key to quit"
+    print ""
+    while True:
+        item = raw_input("Radio ID or IP Address to Convert? ").split(' ')
+        print ""
+        if not item:
+            sys.exit()
+        for id in item:
+            convert(id)
+    
 
-    if '.' in input:
-        id = ip_to_id(input)
-        print ""
-        print "Radio ID is:", id 
-        print ""
-    elif (int(input) < 16777216):
-        ip = id_to_ip(int(input))
-        radio_ip = '12.' + ip
-        pc_ip = '13.' + ip
-        print ""
-        print "Radio IP adddress:       ", radio_ip
-        print "Attached PC IP address:  ", pc_ip
-        print ""
-    else:
-        print ""
-        print "Something went wrong..."
-        print ""
+
+
+
+
+
     
